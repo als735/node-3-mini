3c95f7d20198ace0098fe9ad96538d7b86860f88
Abby Smith <smith@Abbys-MacBook-Pro.local>
2019-05-13 21:03:23 -0600

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+require("dotenv").config();
 const express = require("express");
+const massive = require("massive");
 
 const app = express();
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive(CONNECTION_STRING).then(dbInstance =>{
+  app.set('db', dbInstance); console.log(dbInstance)
+
+  // dbInstance.new_planes()
+  //   .then( planes => console.log( planes ) )
+  //   .catch( err => console.log( err ) );
+
+  dbInstance.get_planes({count : 400})
+    .then(planes => console.log(planes))
+    .catch(err => console.log(err));
+});
 
 app.use(express.json());
 
