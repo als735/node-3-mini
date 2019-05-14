a5e5ed878a7a9150895f89efc3e74c7a11fcb620
Abby Smith <smith@Abbys-MacBook-Pro.local>
2019-05-13 21:04:04 -0600

    second commit

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const app = express();
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 massive(CONNECTION_STRING).then(dbInstance =>{
-  app.set('db', dbInstance); console.log(dbInstance)
+  app.set('db', dbInstance); console.log(dbInstance) // set is allowing you to access dbInstance through a get request using db 
+  // only done once.  
 
   // dbInstance.new_planes()
   //   .then( planes => console.log( planes ) )
   //   .catch( err => console.log( err ) );
 
-  dbInstance.get_planes({count : 400})
-    .then(planes => console.log(planes))
+  dbInstance.get_planes({count : 400}) // accessing the file get_planes and I'm asking for all the planes that have a count above 400 to be returned (generally you want it to run through and end point and then once it hits the end point it will return the data as opposed to the data being returned right off)
+    .then(planes => console.log(planes)) // 
     .catch(err => console.log(err));
 });
 
